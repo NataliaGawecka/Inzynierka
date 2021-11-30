5b693b428a33f00f8f0b63a15c200f7928ef65a6
Natalia Gawęcka <nataliaa0409@gmail.com>
2021-11-30 23:45:12 +0100

    new version

---
+++ Assets/SpawnCar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnCar : MonoBehaviour
+{
+    public Path path;
+    public GameObject car;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(SpawnOneCarOnEachRode());
+    }
+
+     IEnumerator  SpawnOneCarOnEachRode()
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log("list:" + path.AllRoads.Count);
+        for (int x=0; x < path.AllRoads.Count; x++)
+        {
+            Roads_waypoint r=PickRoad( x);
+            Debug.Log("drogi:" + r);
+            Instantiate(car, r.waypoint_nr1.transform.position, car.transform.rotation);
+        }
+     
+    }
+
+    public Roads_waypoint PickRoad(int x)
+    {
+        int i = Random.Range(0, path.AllRoads[x].Count);
+       
+            return path.AllRoads[x][i];
+       
+
+    }
+
+
+  
+
+}
